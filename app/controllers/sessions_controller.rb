0b6d98686fe657e8e119464df79cc73df9a3e411
shimar <shima01dev@gmail.com>
2017-01-05 14:39:44 +0900

    implement authentication.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to(root_path)
+    else
+      render :new
+    end
   end
 
   def destroy
