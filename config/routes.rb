e058f00d8ad5399aa25583b0eb997d950aa545e8
shimar <shima01dev@gmail.com>
2017-01-05 13:57:55 +0900

    add HomeController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root   to: 'home#index'
   get    '/welcome', to: 'sessions#new',     as: :welcome
   post   '/login',   to: 'sessions#create',  as: :login
   delete '/logout',  to: 'sessions#destroy', as: :logout
